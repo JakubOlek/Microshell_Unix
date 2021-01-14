0d4d6a54104a171191de4b5e243249b8d784071c
Veapear <jakub.olek2@gmail.com>
2021-01-14 20:28:52 +0100

    separation space, cd, help , pwd , exit

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -17,12 +18,15 @@ void help() {
     printf("exit"), nextLine();
 }
 
+
 int main() {
 
+
     while (1) {
-        //test
         char userInput[100];
-        scanf("%[^\n]%*c", userInput);
+        printf("[%s:%s]\n$ ", getenv("USER"), getcwd(userInput, 100));
+        scanf(" %[^\n]%*c", userInput);
+
         char *separation = strtok(userInput, " ");
         char *userCommand[sizeof(userInput)];
         int index = 0;
@@ -30,13 +34,20 @@ int main() {
             userCommand[index++] = separation;
             separation = strtok(NULL, " ");
         }
-
-
         if (strcmp(userCommand[0], "help") == 0) {
             nextLine(), help(), nextLine();;
-            printf("[%s]", getcwd(userCommand, 100)), nextLine();
+        } else if (strcmp(userCommand[0], "cd") == 0) {
+            if (strcmp(userCommand[1], "~") == 0) {
+                chdir(getenv("HOME"));
+            } else if (index <= 1) {
+                chdir(getenv("HOME"));
+            } else if (chdir(userCommand[1]) == 0) {
+            } else if (chdir(userCommand[1]) != 0) {
+                printf("cd: %s: No such file or directory", userCommand[1]), nextLine();
+            }
+
         } else if (strcmp(userCommand[0], "pwd") == 0) {
-            printf("[%s]", getcwd(userCommand, 100)), nextLine();
+            printf("%s", getcwd(userInput, 100)), nextLine();
         } else if (strcmp(userCommand[0], "exit") == 0) {
             exit(0);
         }
