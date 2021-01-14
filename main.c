4fe15a424fd8ed0d3b819d402a306dad19130167
Veapear <jakub.olek2@gmail.com>
2021-01-14 14:04:29 +0100

    separation space, help , pwd , exit

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 
 void nextLine() {
     printf("\n");
 }
-
 
 void help() {
     printf("*** MicroShell SOP ***"), nextLine();
@@ -21,38 +18,30 @@ void help() {
 }
 
 int main() {
-    char userCommand[100];
-    char supportCd[3];
-    printf("[%s]", getcwd(userCommand, 100)), nextLine();
-
 
     while (1) {
-        printf("%s", "$ ");
-        scanf("%[^\n]%*c", userCommand);
-        strncpy(supportCd, userCommand, 2);
-        char *changeDir = userCommand + 3;
 
-        if (strcmp(userCommand, "help") == 0) {
+        char userInput[100];
+        scanf("%[^\n]%*c", userInput);
+        char *separation = strtok(userInput, " ");
+        char *userCommand[sizeof(userInput)];
+        int index = 0;
+        while (separation != NULL) {
+            userCommand[index++] = separation;
+            separation = strtok(NULL, " ");
+        }
+
+
+        if (strcmp(userCommand[0], "help") == 0) {
             nextLine(), help(), nextLine();;
             printf("[%s]", getcwd(userCommand, 100)), nextLine();
-        } else if (strcmp(supportCd, "cd") == 0) {
-            if (strcmp(userCommand, "cd") == 0) {
-                chdir(getenv("HOME"));
-                printf("[%s]", getcwd(userCommand, 100)), nextLine();
-            } else if (strcmp(changeDir, "~") == 0) {
-                chdir(getenv("HOME"));
-                printf("[%s]", getcwd(userCommand, 100)), nextLine();
-            } else {
-                chdir(changeDir);
-                printf("[%s]", getcwd(userCommand, 100)), nextLine();
-            }
-        } else if (strcmp(userCommand, "pwd") == 0) {
+        } else if (strcmp(userCommand[0], "pwd") == 0) {
             printf("[%s]", getcwd(userCommand, 100)), nextLine();
-        } else if (strcmp(userCommand, "exit") == 0) {
+        } else if (strcmp(userCommand[0], "exit") == 0) {
             exit(0);
-        } else {
-            printf("Command '%s' not found", userCommand), nextLine();
         }
+
+
     }
     return 0;
 }
